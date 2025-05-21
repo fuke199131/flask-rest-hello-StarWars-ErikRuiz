11abd992c19060dfb54f04ebf0936113f64bb277
The_NorthernFox Aka Erik Ruiz <erikruiz13@gmail.com>
2025-05-21 19:47:06 +0000

    Proyecto finalizado star wars

--- src/models.py
+++ src/models.py
@@ -1,19 +1,52 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Enum
+from sqlalchemy.orm import relationship, declarative_base
+import enum
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class TipoFavoritoEnum(enum.Enum):
+    personaje = "personaje"
+    planeta = "planeta"
 
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key=True)
+    correo = Column(String(120), unique=True, nullable=False)
+    contraseña = Column(String(80), nullable=False)
+    nombre = Column(String(120))
+    apellido = Column(String(120))
+    fecha_suscripcion = Column(DateTime)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    favoritos = relationship("Favorito", back_populates="usuario")
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    genero = Column(String(50))
+    año_nacimiento = Column(String(50))
+    color_ojos = Column(String(50))
+
+    favoritos = relationship("Favorito", back_populates="personaje")
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    clima = Column(String(100))
+    terreno = Column(String(100))
+    poblacion = Column(String(100))
+
+    favoritos = relationship("Favorito", back_populates="planeta")
+
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'), nullable=False)
+    personaje_id = Column(Integer, ForeignKey('personaje.id'), nullable=True)
+    planeta_id = Column(Integer, ForeignKey('planeta.id'), nullable=True)
+    tipo = Column(Enum(TipoFavoritoEnum), nullable=False)
+
+    usuario = relationship("Usuario", back_populates="favoritos")
+    personaje = relationship("Personaje", back_populates="favoritos")
+    planeta = relationship("Planeta", back_populates="favoritos")
